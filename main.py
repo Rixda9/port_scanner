c471c65a6be923d62dae323ed7113fe1f714f361
Rixda9 <78431473+Rixda9@users.noreply.github.com>
2026-03-08 16:44:08 +0100

    v2 - threaded port scanner with banners

---
+++ main.py
@@ -0,0 +1,50 @@
+import socket 
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+from ipaddress import ip_address
+
+lock = Lock()
+open_ports = []
+
+def port_test(ip, port, timeout=3):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(timeout)
+        if sock.connect_ex((ip, port)) == 0:
+            try:
+                # Infos about the port
+                banner = sock.recv(1024).decode().strip()
+            except:
+                banner = "no banner"
+
+            print(f"{port} is open - {banner}")
+            # Avoid race conditions
+            with lock:
+                open_ports.append(port)
+            
+        
+def ip_to_check(ip):
+    try:
+        ip_address(ip) 
+        return ip
+    except ValueError:
+        raise ValueError("invalid ip")
+
+def main():
+
+    ip = ip_to_check(input("What ip do you want to test: "))
+    try:
+        start = int(input("Start port: "))
+        end = int(input("End port: "))
+        if start >= end:
+            raise ValueError("Start port must be less than end port")
+    except ValueError:
+        raise ValueError("Enter valid numbers: ")
+        
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        executor.map(lambda p: port_test(ip, p), range(start, end + 1))
+    print(open_ports)
+
+if __name__ = "__main__":
+main()
+
+
